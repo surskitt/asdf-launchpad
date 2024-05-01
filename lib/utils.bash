275172dd95a5fe9b717b6e6388386cd53e2576c3
surskitt <surskitt@users.noreply.github.com>
2024-05-01 17:11:46 +0100

    feat: first version of working install

--- lib/utils.bash
+++ lib/utils.bash
@@ -5,19 +5,19 @@ set -euo pipefail
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for launchpad.
 GH_REPO="https://github.com/Mirantis/launchpad"
 TOOL_NAME="launchpad"
-TOOL_TEST="launchpad version"
+# TOOL_TEST="launchpad version"
 
 fail() {
 	echo -e "asdf-$TOOL_NAME: $*"
 	exit 1
 }
 
-curl_opts=(-fsSL)
+# curl_opts=(-fsSL)
 
 # NOTE: You might want to remove this if launchpad is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
+# if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+# 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+# fi
 
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
@@ -31,44 +31,81 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if launchpad has other means of determining installable versions.
 	list_github_tags
 }
 
-download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
+# download_release() {
+# 	local version filename url
+# 	version="$1"
+# 	filename="$2"
+#
+# 	# TODO: Adapt the release URL convention for launchpad
+# 	url="$GH_REPO/archive/v${version}.tar.gz"
+#
+# 	echo "* Downloading $TOOL_NAME release $version..."
+# 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+# }
 
-	# TODO: Adapt the release URL convention for launchpad
-	url="$GH_REPO/archive/v${version}.tar.gz"
+# install_version() {
+# 	local install_type="$1"
+# 	local version="$2"
+# 	local install_path="${3%/bin}/bin"
+#
+# 	if [ "$install_type" != "version" ]; then
+# 		fail "asdf-$TOOL_NAME supports release installs only"
+# 	fi
+#
+# 	(
+# 		mkdir -p "$install_path"
+# 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+#
+# 		# TODO: Assert launchpad executable exists.
+# 		local tool_cmd
+# 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+# 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+#
+# 		echo "$TOOL_NAME $version installation was successful!"
+# 	) || (
+# 		rm -rf "$install_path"
+# 		fail "An error occurred while installing $TOOL_NAME $version."
+# 	)
+# }
 
-	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+get_platform() {
+	platform="$(uname -s)"
+
+	case "${platform}" in
+		Darwin|FreeBSD|Linux|Windows) ;;
+		*) fail "Platform ${platform} unsupported" ;;
+	esac
+
+	arch="$(uname -m)"
+
+	case "${arch}" in
+		x86_64)
+			arch="amd64"
+			;;
+		arm64)
+			arch="arm64"
+			;;
+		*)
+			fail "Arch ${arch} unsupported"
+			;;
+	esac
+
+	echo "${platform,,}_${arch}"
 }
 
-install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="${3%/bin}/bin"
+get_download_url() {
+	version="${1}"
 
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
+	platform="$(get_platform)"
+
+	url="https://get.mirantis.com/launchpad/v${version}/launchpad_${platform}_${version}"
+
+	if [[ "${platform}" = "windows"* ]] ; then
+		url="${url}.exe"
 	fi
 
-	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
-
-		# TODO: Assert launchpad executable exists.
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
-
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
+	echo "${url}"
 }
